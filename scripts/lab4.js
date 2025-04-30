41fe7859062b76ccdcc0288680a439f40826e189
ltsamaan <130004821+ltsamaan@users.noreply.github.com>
2025-04-29 21:27:11 -0700

    Update lab4.js with even more fixes

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -25,6 +25,10 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length === 0 || isNaN(discount)) {
+        return false;
+    }
+    
     const discounted = []
     const length = prices.length;
     
